0139503af13c6109c1cabef79e98ee7985bb2b9a
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-04 18:41:14 +0600

    Deletion Without STL

---
+++ 1. Array [Insertion-Deletion-Update_Element]/3. Deletion.cpp
@@ -0,0 +1,31 @@
+/*//  بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  //*/
+/*//   Md Sumon Hossain Khan   //*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int index;
+    cin >> index;
+
+    for(int i=index; i<n; i++){
+        arr[i] = arr[i+1];
+    }
+    n-=1;
+
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+
+
+    return 0;
+}
